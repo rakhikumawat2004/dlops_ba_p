52f8dc00792ef593bb271dfe1849a1b7e1ed2656
Shweta Kumawat <shwetakumawat2112@gmail.com>
2026-04-29 09:39:41 +0530

    added

---
+++ main.py
@@ -0,0 +1,25 @@
+# from src.cnnClassifier import logger 
+# import logging 
+# print(logging.info("Welcome to ML Batch P logs"))
+
+from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline 
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+STAGE_NAME = "Prepare Base Model"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
